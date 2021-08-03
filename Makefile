97fa730498bd0171dc35ae84241acd2be132a852
Zhi Fan <chenzhifan9@gmail.com>
2021-08-03 10:22:16 +0200

    Added test, support for venv for pyright and Makefile for testing

---
+++ Makefile
@@ -0,0 +1,3 @@
+test:
+	python3 -m unittest discover -s unimi_dl/tests -p "test_*.py"
+
---
+++ unimi_dl/tests/test_ariel.py
@@ -0,0 +1,15 @@
+import unittest
+
+class TestAriel(unittest.TestCase):
+    def test_prova(self):
+        self.assertEqual('foo', True)
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+if __name__ == '__main__':
+    unittest.main()
